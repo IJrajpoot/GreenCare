b7e3eb82354cfb19660510fafbf456e4128d81fd
Iqra Jawaid <sp21bsse0021@maju.edu.pk>
2024-10-24 22:09:35 +0500

    Updated: topbar.dart

--- lib/topbar.dart
+++ lib/topbar.dart
@@ -4,10 +4,11 @@ import 'menu.dart';
 
 class TopBar extends StatelessWidget {
   const TopBar({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.fromLTRB(16.0, 32.0, 16.0, 5.0),
       color: const Color(0xFF3C7A17),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -29,19 +30,44 @@ class TopBar extends StatelessWidget {
               style: TextStyle(color: Colors.white, fontSize: 18),
             ),
           ),
-          InkWell(
-            onTap: () {
-              // Navigate to the ManageProfileScreen
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const ManageProfileScreen()),
-              );
-            },
-            child: const CircleAvatar(
+          PopupMenuButton<String>(
+            icon: const CircleAvatar(
               backgroundImage: AssetImage('assets/icons/avatar.png'),
               backgroundColor: Colors.white,
               radius: 20,
             ),
+            onSelected: (String value) {
+              if (value == 'Edit Profile') {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const ManageProfileScreen()),
+                );
+              } else if (value == 'Logout') {
+                // Handle logout functionality
+                print('User logged out');
+                // Add your logout logic here
+              }
+            },
+            itemBuilder: (BuildContext context) => [
+              PopupMenuItem<String>(
+                value: 'Edit Profile',
+                child: ListTile(
+                  leading: const Icon(Icons.edit, color: Colors.black),
+                  title: const Text('Edit Profile'),
+                ),
+              ),
+              PopupMenuItem<String>(
+                value: 'Logout',
+                child: ListTile(
+                  leading: const Icon(Icons.logout, color: Colors.red),
+                  title: const Text(
+                    'Logout',
+                    style: TextStyle(color: Colors.red),
+                  ),
+                ),
+              ),
+            ],
           ),
         ],
       ),
