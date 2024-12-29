2d4ce1c07bc6c4385a60e45063b8133f79e10e9e
Iqra Jawaid <sp21bsse0021@maju.edu.pk>
2024-12-29 23:02:31 +0500

    Undo: Removed all unnecessary and deprecated packages and dependencies

--- lib/detected_disease.dart
+++ lib/detected_disease.dart
@@ -1,19 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:GreenCare/navbar.dart';
-import 'package:GreenCare/topbar.dart';
-import 'dart:io';
+import 'package:greencare/constans/colors.dart';
+import 'package:greencare/navbar.dart';
+import 'package:greencare/topbar.dart';
 
 class DetectedDiseaseScreen extends StatefulWidget {
-  final String imagePath;
-  final String predictedLabel;
-  final double accuracy;
-
-  const DetectedDiseaseScreen({
-    super.key,
-    required this.imagePath,
-    required this.predictedLabel,
-    required this.accuracy,
-  });
+  const DetectedDiseaseScreen({super.key});
 
   @override
   State<DetectedDiseaseScreen> createState() => _DetectedDiseaseScreenState();
@@ -23,8 +14,8 @@ class _DetectedDiseaseScreenState extends State<DetectedDiseaseScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const PreferredSize(
-        preferredSize: Size.fromHeight(80.0),
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(80.0),
         child: TopBar(),
       ),
       body: SingleChildScrollView(
@@ -34,64 +25,118 @@ class _DetectedDiseaseScreenState extends State<DetectedDiseaseScreen> {
               SizedBox(
                 height: 250,
                 width: MediaQuery.of(context).size.width,
-                child: Image.file(
-                  File(widget.imagePath),
+                child: Image.asset(
+                  'assets/images/wheat.jpg',
                   fit: BoxFit.cover,
-                  errorBuilder: (context, error, stackTrace) {
-                    return const Text('Error loading image');
-                  },
+                  // width: double.infinity,
                 ),
               ),
-              const SizedBox(height: 20),
+              const SizedBox(
+                height: 20,
+              ),
               Container(
                 width: double.infinity,
                 margin: const EdgeInsets.symmetric(horizontal: 20),
-                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                padding:
+                const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(20),
                   color: Colors.grey.shade300,
                 ),
                 child: Column(
                   children: [
-                    Text(
-                      widget.predictedLabel,
-                      style: const TextStyle(
+                    const Text(
+                      'Black Chaff',
+                      style: TextStyle(
                         fontSize: 26,
-                        color: Color(0xFF3C7A17),
+                        color: primaryGreen,
                         fontWeight: FontWeight.w800,
                       ),
                     ),
                     const Text(
-                      'Crop: Wheat', // Adjust based on your label mapping
-                      style: TextStyle(fontSize: 16),
+                      'Crop: Wheat',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
                     ),
-                    Text(
-                      'Detection Accuracy: ${widget.accuracy.toStringAsFixed(2)}%',
-                      style: const TextStyle(fontSize: 16),
+                    const Text(
+                      'Detection Accuracy: 95%',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
                     ),
                     Align(
                       alignment: Alignment.bottomRight,
                       child: TextButton(
-                        onPressed: () {},
-                        child: const Text(
-                          'Read More ..',
-                          style: TextStyle(
-                            color: Color(0xFF3C7A17),
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                      ),
-                    ),
+                          onPressed: () {},
+                          child: const Text(
+                            'Read More ..',
+                            style: TextStyle(
+                                color: primaryGreen,
+                                fontWeight: FontWeight.w500),
+                          )),
+                    )
                   ],
                 ),
               ),
-              const SizedBox(height: 40),
-              // Other buttons
+              const SizedBox(
+                height: 40,
+              ),
+              Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(vertical: 16.0),
+                        backgroundColor: primaryGreen,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                      ),
+                      child: const Text(
+                        'Get First Aid',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  )),
+              const SizedBox(
+                height: 20,
+              ),
+              Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(vertical: 16.0),
+                        backgroundColor: primaryGreen,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                      ),
+                      child: const Text(
+                        'Save Results',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  )),
+              const SizedBox(
+                height: 60,
+              )
             ],
           ),
         ),
       ),
-      bottomNavigationBar: const NavBar(),
+      bottomNavigationBar: NavBar(),
     );
   }
 }
