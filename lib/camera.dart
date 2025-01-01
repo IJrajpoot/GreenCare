7013b0813eb9952775d47749b9b664fd8f92ad74
Iqra Jawaid <sp21bsse0021@maju.edu.pk>
2025-01-02 00:04:04 +0500

    Integrated: Successfully integrated server in camera.dart

--- lib/camera.dart
+++ lib/camera.dart
@@ -2,11 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class CameraPage extends StatelessWidget {
-  final String url = 'https://iqrajawaid-cropdiseasedetector.hf.space/';
+  final String url = 'https://iqrajawaid-cropdiseasedetector.hf.space';
 
   Future<void> _launchURL() async {
-    if (await canLaunch(url)) {
-      await launch(url);
+    final Uri uri = Uri.parse(url);
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(
+        uri,
+        mode: LaunchMode.externalApplication, // Opens in a browser
+      );
     } else {
       throw 'Could not launch $url';
     }
